776ec4ead8aa9362b234779a093650888efbdc09
louisboii747 <louishinchliffe11@gmail.com>
2026-05-10 18:30:02 +0100

    Refactor import order in api.py for improved readability

--- hardwaremon/api.py
+++ hardwaremon/api.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
-
-import psutil
-import GPUtil
 import platform
 import time
+
+import GPUtil
+import psutil
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
@@ -58,6 +58,7 @@ def stats():
             .strip()
         )
     except:
+        exception = "Unknown CPU"
         cpu_name = platform.processor()
 
     ram_total = round(
